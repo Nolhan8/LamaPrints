9ec5e0ae58b1a01eac16513bde2051bf3ce039d6
Nolhan8 <nolhancalon@gmail.com>
2024-05-02 12:23:06 +0200

    Small update

---
+++ public/js/scripts.js
@@ -0,0 +1,4 @@
+    // Script pour activer/désactiver le menu déroulant en version mobile
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        document.getElementById('menu-content').classList.toggle('show');
+    });
